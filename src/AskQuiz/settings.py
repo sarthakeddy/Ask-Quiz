8e15ae9074b8ce21253f0f1da01751e1542a8d9d
sarthakeddy <sarthakshukla.eddy@gmail.com>
2020-07-31 00:10:01 +0530

    Add starting page

--- src/AskQuiz/settings.py
+++ src/AskQuiz/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     # user-created
     'questions',
+    'pages',
 ]
 
 MIDDLEWARE = [
@@ -57,7 +58,7 @@ ROOT_URLCONF = 'AskQuiz.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
